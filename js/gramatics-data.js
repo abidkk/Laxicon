e5335a6aa883fd81e37b089e3bba21f2e8908542
abidkk <dev.abidkk@gmail.com>
2026-08-20 10:01:20 +0500

    gramatica

---
+++ js/gramatics-data.js
@@ -0,0 +1,28 @@
+// ===== Gramática data =====
+// One object per lesson, in the order you want them to appear.
+// Each topic links out to your notes (Drive link, doc, etc).
+const GRAMATICA_DATA = [
+  {
+    lesson: "Lesson 1: Parts of Speech",
+    topics: [
+      { title: "1. The Noun", link: "https://drive.google.com/file/d/1aj7H4Qn4vO-6ShCQxIiQ_zKRizfmOUOp/view?usp=drive_link" },
+      { title: "2. The Pronoun", link: "https://drive.google.com/file/d/1rvV2uujzCMMPhq0Hbnuc_b1y5Y2VZVqQ/view?usp=drive_link" },
+      { title: "3. The Verbs", link:"https://drive.google.com/file/d/17coE-8lSmdUzBpYFks_aIMP_Eu1Mskcs/view?usp=drive_link"},
+      { title: "4. The Adjectives", link:"https://drive.google.com/file/d/1QYVa_Zh8fFbHsGvCOsGXOT6UTrzQffiZ/view?usp=drive_link"},     
+      { title: "5. The Adverbs", link:"https://drive.google.com/file/d/1JXtJmMdNEE_hBT1apbHZZmJxLSd_nhFZ/view?usp=drive_link"},
+      { title: "6. The Prepositions", link:"https://drive.google.com/file/d/1-5xZFi-XCXoHmL6pa3GS5FmTyP2az7ux/view?usp=drive_link"},
+      { title: "", link:""},
+    ],
+  },
+
+    {
+    lesson: "Lesson 2: Tenses Active & Passive",
+    topics: [
+      { title: "1. The Noun", link: "drive link" },
+      { title: "2. The Pronoun", link: "drive link" },
+    ],
+  },
+
+
+
+];
